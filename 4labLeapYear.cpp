60091573e9f7a52175c86ce40abd457919aec1d5
gabesilsby <silsbyjg467@mail.sandhills.edu>
2025-09-13 12:54:49 -0400

    Committed at work 13th

--- 4labLeapYear.cpp
+++ 4labLeapYear.cpp
@@ -12,13 +12,6 @@ then print "The year you entered xxxx is NOT leap year"
 1. The year is a multiple of 400.
 2. The year is a multiple of 4 but not a multiple of 100.
 */
-
-/*
-Determine whether the year is divisible by 100. If it is, then it is a leap year if and
-only if it is divisible by 400. For example, 2000 is a leap year but 2100 is not.
-2. If the year is not divisible by 100, then it is a leap year if and if only it is divisible by
-4. For example, 2008 is a leap year but 2009 is not.
-*/
 #include <iostream>
 
 using namespace std;
@@ -30,23 +23,22 @@ int main()
     cout<<"Enter a year: ";
     cin>>year;
 
-    if (year % 100 ==0)
-        cout<<"is leapy";
-        if (year % 400 == 0)
-        {
-            cout<<"is leapy";
-                }
-        else 
-            cout<<"notleapy";
-    else if (year % 100 != 0)
-        if (year % 4==0)
-        {
-            cout<<"leapy";
-        }
-    else 
-        cout<<"not elly";
-        
+    if (year%4==0)
+    {
+        if (year%100==0)
+            {
+                if (year%400==0)
+                    cout<<"The year you entered: " << year <<" is a leap year"<<endl;
+                else 
+                    cout<<"The year you entered: " << year <<" is NOT a leap year"<<endl;
+
+            }
+        else
+            cout<<"The year you entered: " << year << " is a leap year"<<endl;
+
+    }
+    else
+        cout<<"The year you entered: " << year <<" is NOT a leap year"<<endl;
 
     return 0;
-
 }
