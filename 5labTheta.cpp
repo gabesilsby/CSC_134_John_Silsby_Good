a8a5265a16b7323b5bf77f912c5a058ad8580fe9
gabesilsby <silsbyjg467@mail.sandhills.edu>
2025-09-17 11:01:51 -0400

    week5inclass

---
+++ 5labTheta.cpp
@@ -0,0 +1,42 @@
+//@author: 
+//@date: 
+//@purpose:
+/*
+Design and write a program that calculates the angle(theta) of a right triangle given the following:
+length of triangle x is 3 units
+height of triangle y is 4 units
+The angle theta is adjacent to leg x
+use the equation 𝑥2+𝑦2=𝑐2 , let c equal the hypotenuse
+Note: use the atan2() function, to convert from radians to degrees use 180/𝜋. Use 
+ChatGPT to research the atan2() function and how it can be used in your program.
+If the angle(theta) is equals 53.13° then print "This is a 3-4-5 triangle", otherwise 
+print "This is NOT a 3-4-5 triangle".
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int lengthTriangle = 3;
+    int heightTriangle = 4;
+
+    double thetaRad = atan2(heightTriangle, lengthTriangle);
+
+    //converting theta to degrees
+    double thetaDeg = thetaRad*(180/3.1415926536);
+
+    cout<<thetaDeg<<endl;
+
+    if (thetaDeg >= 53.13 && thetaDeg <= 53.139)
+        {cout<<"good stuff\n these dimensions are a 3-4-5 triangle";}
+    else
+        {cout<<"bad stuff\n this is NOT a 3-4-5 triangle";}
+
+    
+
+    return 0;
+
+}
