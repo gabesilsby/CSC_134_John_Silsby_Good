6309f8b26dd8a8f87d6a505e732ce22a744c3bf7
gabesilsby <silsbyjg467@mail.sandhills.edu>
2025-10-24 18:32:00 -0400

    Commit on RDU drive

--- 9labSales.cpp
+++ 9labSales.cpp
@@ -15,24 +15,37 @@ using namespace std;
 
 int main()
 {
+    //creates an output file stream and opens the file named "week9_lab1.txt"
     ofstream outputFile("week9_lab1.txt");
 
+    //declares variables and initializes totalSales as 0
     int numOfDays;
     float daySales;
     float totalSales = 0;
+
     cout<<"How many days: ";
+    //recieves user input
     cin>>numOfDays;
 
+    //initializes i as 0; when i is less than numOfDays, the condition is true and 
+    //the code in the brackets {} will run; add 1 value to i each loop
     for (int i = 0; i < numOfDays; i++)
     {
+        //writes to terminal to prompt user to enter daySales
         cout<<"Sales for today: ";
         cin>>daySales;
+        //writes the dollar amount of what the user entered 
         outputFile<<"$"<<daySales<<endl;
+        //totalSales is equal to totalSales + daySales
         totalSales += daySales;
     }
+    //(I wrote the totalSales feature before reading what lab 2 wanted)
+    //writes the totalSales value to buffer and then the ouput text file once the for loop is satisfied
     outputFile<<"$"<<totalSales<<endl;
     
+    //flushes buffer's data to disk
     outputFile.close();
+    //lets terminal know that the program is run
     cout<<"finished"<<endl;
 
     return 0;
--- 9labSalesTotal.cpp
+++ 9labSalesTotal.cpp
@@ -28,30 +28,47 @@ using namespace std;
 
 int main()
 {
+    //creates an output file stream and opens the file named "week9_lab2.txt"
     ofstream outputFile("week9_lab2.txt");
 
+    //declaring variables
     int numOfDays;
     float daySales;
+    //i initialized this one because of industry practice, I'm not actually sure what separates these from the others
     float totalSales = 0;
     cout<<"How many days: ";
+    //recieve user input
     cin>>numOfDays;
 
+    //initializes i as 0; when i is less than numOfDays, the condition is true and the code in the brackets 
+    //{} will run; add 1 value to i each loop
     for (int i = 0; i < numOfDays; i++)
     {
+        //outputs text in terminal
         cout<<"Sales for day "<<i+1<<": ";
+        //recieves user input
         cin>>daySales;
+        //write text onto the file through internal buffer; when endl is ran it flushes the buffer (apparently)
         outputFile<<"Day "<<i+1;
+        //fixed tells fstream that the notation is standard decimal notation, and setprecion(#) tells 
+        //fstream # many decimal points(must be float or double)
         outputFile<<fixed<<setprecision(2);
         outputFile<<" $"<<daySales<<endl;
+        //totalSales is equal to totalSales plus daySales
         totalSales += daySales;
     }
 
+    //writes the totalSales value to buffer and then the ouput text file once the for loop is satisfiedd
+    //fixed tells fstream that the notation is standard decimal notation, and setprecion(#) 
+    //tells fstream # many decimal points(must be float or double)
     outputFile<<fixed<<setprecision(2);
     outputFile<<"======================"<<endl;
     outputFile<<"Total Sales: $"<<totalSales<<endl;
     outputFile<<"======================"<<endl;
     
+    //flushes buffer to disk, might be redundant because of the endl line above
     outputFile.close();
+    //writes in terminal that the code has run
     cout<<"finished"<<endl;
 
     return 0;
